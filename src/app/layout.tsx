cc6180315f630a3d766ab59030b44a5e73843816
danishyarkhwork <danishyarkhwork@gmail.com>
2024-08-20 15:27:25 +0430

    fix: the Header, Footer and Hero section cretaed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,9 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script";
+import Header from "@/components/layout/Header";
+import Footer from "@/components/layout/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +19,27 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <link
+        href="assets/libs/swiper/swiper-bundle.min.css"
+        rel="stylesheet"
+        type="text/css"
+      />
+      <link href="assets/libs/aos/aos.css" rel="stylesheet" type="text/css" />
+
+      <link href="assets/css/style.min.css" rel="stylesheet" type="text/css" />
+
+      <link href="assets/css/icons.min.css" rel="stylesheet" type="text/css" />
+
+      <body className={inter.className}>
+        <Header />
+        {children}
+        <Footer />
+      </body>
+
+      <Script src="assets/libs/@frostui/tailwindcss/frostui.js"></Script>
+      <Script src="assets/libs/swiper/swiper-bundle.min.js"></Script>
+      <Script src="assets/libs/aos/aos.js"></Script>
+      <Script src="assets/js/theme.min.js"></Script>
     </html>
   );
 }
