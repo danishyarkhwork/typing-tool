12079d26a5b1c94c3bf7c7c09a3882a5092123ef
danishyarkhwork <danishyarkhwork@gmail.com>
2024-08-23 20:13:18 +0430

    fix: the PWA functionality removed

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,4 @@
-// next.config.mjs
-import withPWA from "next-pwa";
+/** @type {import('next').NextConfig} */
+const nextConfig = {};
 
-const nextConfig = {
-  // Other Next.js config options can go here
-};
-
-export default withPWA({
-  dest: "public",
-  register: true,
-  skipWaiting: true,
-})(nextConfig);
+export default nextConfig;
